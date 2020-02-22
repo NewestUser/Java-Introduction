c3d6fc6261365eb8b72e329ecd9c1bd6184b0a33
Marian Zlatev <mzlatev91@gmail.com>
2020-02-22 21:51:55 +0200

    lecture5: add java code for non complete solution of task 4 (spiral matrix)

--- src/lecture5/Homework5.java
+++ src/lecture5/Homework5.java
@@ -302,6 +302,59 @@ public class Homework5 {
     }
 
     /**
+     * TODO (mzlatev) working solution from a student. Rework it so that it populates the value of the matrix
+     */
+    public static void task4Main(String[] args) {
+        int R = 4;
+        int C = 4;
+        int a[][] = {{1, 2, 3, 4},
+                {12, 13, 14, 5},
+                {11, 16, 15, 6},
+                {10, 9, 8, 7}};
+        spiralPrint(R, C, a);
+    }
+
+    static void spiralPrint(int m, int n, int a[][]) {
+        int i, k = 0, l = 0;
+        /*  k - starting row index
+        m - ending row index
+        l - starting column index
+        n - ending column index
+        i - iterator
+        */
+
+        while (k < m && l < n) {
+            // Print the first row from the remaining rows
+            for (i = l; i < n; ++i) {
+                System.out.print(a[k][i] + " ");
+            }
+            k++;
+
+            // Print the last column from the remaining columns
+            for (i = k; i < m; ++i) {
+                System.out.print(a[i][n - 1] + " ");
+            }
+            n--;
+
+            // Print the last row from the remaining rows */
+            if (k < m) {
+                for (i = n - 1; i >= l; --i) {
+                    System.out.print(a[m - 1][i] + " ");
+                }
+                m--;
+            }
+
+            // Print the first column from the remaining columns */
+            if (l < n) {
+                for (i = m - 1; i >= k; --i) {
+                    System.out.print(a[i][l] + " ");
+                }
+                l++;
+            }
+        }
+    }
+
+    /**
      * Да се завърти двумерен масив (за по-лесно квадратен с четен брой елементи – 4х4, 6х6 и т.н.)
      * по часовниковата стрелка:
      * Например за дадената матрица:
